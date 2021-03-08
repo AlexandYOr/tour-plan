369ca54370ff9c12e6f8f533a0430f4d59aa9f95
AlexandYOr <avo01121997@yandex.ru>
2021-03-08 17:32:11 +0300

    crete: change slides using the keyboard

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
+  // Управление слайдами с помощью клавиатуры
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  
 });
