c1aa6c646868507481086ff85c71db83ff60fed7
AlexandYOr <avo01121997@yandex.ru>
2021-03-10 17:53:13 +0300

    create : slider35^26

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Управление слайдами с помощью клавиатуры
@@ -9,8 +9,24 @@ const swiper = new Swiper('.swiper-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Управление слайдами с помощью клавиатуры
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   
 });
