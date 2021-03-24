1d9d94fbbdd6a325afccff62ac8aeeef7ef490f0
AlexandYOr <avo01121997@yandex.ru>
2021-03-24 14:44:43 +0300

    Optimization

--- js/main.js
+++ js/main.js
@@ -36,40 +36,88 @@ var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function () {
   document.querySelector(".navbar-bottom")
     .classList.toggle('navbar-bottom--visible')
-})
-  
-  var modalButton = $('[data-toggle=modal]');
-  var closeModalButton = $('.modal__close');
-  var modalOverlay = $(".modal__overlay");
-  var modalDialog = $(".modal__dialog");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
-
-  function openModal() {
-    modalOverlay.addClass("modal__overlay--visible");
-    modalDialog.addClass("modal__dialog--visible");
-    $("body").css("overflow","hidden");
-  }
-  function closeModal(event) {
-    event.preventDefault();
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-    $("body").css("overflow","scroll");
-  }
-  $(document).keydown(function(e) {
-    if (e.keyCode == 27) {
+}) 
+const modalButton = $("[data-toggle=modal]");
+    const closeModalButton = $(".modal__close");
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    const openModal = () => {
+      modalOverlay.addClass("modal__overlay--visible");
+      modalDialog.addClass("modal__dialog--visible");
+      $("body").css("overflow","hidden");
+      bodyFixPosition();
+    };
+    const closeModal = event => {
+      event.preventDefault();
       modalOverlay.removeClass("modal__overlay--visible");
       modalDialog.removeClass("modal__dialog--visible");
       $("body").css("overflow","scroll");
-    }
-  });
-  $(document).click(function (e) {
-    if ($(e.target).is('.modal__overlay')) {
+      bodyUnfixPosition();
+    };
+    const turnModal = () => {
       modalOverlay.removeClass("modal__overlay--visible");
       modalDialog.removeClass("modal__dialog--visible");
-      $("body").css("overflow","scroll")
+      $("body").css("overflow","scroll");
+      bodyUnfixPosition();
+    };
+    $(document).keydown(event => {
+      const key = event.key;
+      if (key === "Escape") {
+        turnModal();
+      }
+    });
+    $(document).click(event => {
+      if ($(event.target).is('.modal__overlay')) {
+        turnModal();
+      }
+    });
+    modalButton.on('click', openModal);
+    closeModalButton.on('click', closeModal);
+    const bodyFixPosition = () => {
+
+      setTimeout(() => {
+      /* Ставим необходимую задержку, чтобы не было «конфликта» в случае, если функция фиксации вызывается сразу после расфиксации (расфиксация отменяет действия расфиксации из-за одновременного действия) */
+        if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
+
+          // Получаем позицию прокрутки
+          let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    
+          // Ставим нужные стили
+          document.body.setAttribute('data-body-scroll-fix', scrollPosition); // Cтавим атрибут со значением прокрутки
+          document.body.style.overflow = 'hidden';
+          document.body.style.position = 'fixed';
+          document.body.style.top = '-' + scrollPosition + 'px';
+          document.body.style.left = '0';
+          document.body.style.width = '100%';
+    
+        }
+    
+      }, 15 ); /* Можно задержку ещё меньше, но у меня работало хорошо именно с этим значением на всех устройствах и браузерах */
+    
     }
-  });
+    
+    // 2. Расфиксация <body>
+    function bodyUnfixPosition() {
+    
+      if ( document.body.hasAttribute('data-body-scroll-fix') ) {
+    
+        // Получаем позицию прокрутки из атрибута
+        let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+    
+        // Удаляем атрибут
+        document.body.removeAttribute('data-body-scroll-fix');
+    
+        // Удаляем ненужные стили
+        document.body.style.overflow = '';
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.left = '';
+        document.body.style.width = '';
+    
+        // Прокручиваем страницу на полученное из атрибута значение
+        window.scroll(0, scrollPosition);
+      }
+    }
   // обработка форм
   $(".form").each(function(){
     $(this).validate({
